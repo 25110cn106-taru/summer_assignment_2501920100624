074ac4413f27bf62bdececaeb8339f2f2004d99d
25110cn106-taru <25110cn106@glbitm.ac.in>
2026-06-22 22:31:17 +0530

    Add files via upload

---
+++ DAY-20/check_symmetric_matrix.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class check_symmetric_matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns for the matrix:");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        boolean isSymmetric = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    isSymmetric = false;
+                    break;
+                }
+            }
+            if (!isSymmetric) {
+                break;
+            }
+        }
+
+        if (isSymmetric) {
+            System.out.println("The matrix is symmetric.");
+        } else {
+            System.out.println("The matrix is not symmetric.");
+        }
+
+        sc.close();
+    }
+}
---
+++ DAY-20/rowwise_sum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class rowwise_sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows for the matrix:");
+        int r = sc.nextInt();
+        System.out.println("Enter the number of columns for the matrix:");
+        int c = sc.nextInt();
+
+        int[][] matrix = new int[r][c];
+
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Row-wise sum of the matrix:");
+        for (int i = 0; i < r; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < c; j++) {
+                rowSum += matrix[i][j];
+            }
+            System.out.println("Sum of row " + (i + 1) + ": " + rowSum);
+        }
+
+        sc.close();
+    }
+    
+}
